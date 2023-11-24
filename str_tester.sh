8d5e03dd98533ecfa5f66c4761485c4295dcc8c4
lgaume <lorisprog@gmail.com>
2023-11-24 11:37:35 +0100

    removing useless test3

--- str_tester.sh
+++ str_tester.sh
@@ -110,7 +110,7 @@ printf "FT_STRLCPY :  "
 #tests $STRLCPY $LCPY 5
 printf "\n"
 printf "FT_STRLEN :   "
-tests $STRLEN $LEN 3
+tests $STRLEN $LEN 2
 printf "\n"
 printf "FT_STRMAPI :  "
 #tests $STRMAPI $MAPI 5
