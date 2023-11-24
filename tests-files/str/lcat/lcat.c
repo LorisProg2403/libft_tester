e972dbdc0a90db516933030e68cbdc63259a263c
lgaume <lorisprog@gmail.com>
2023-11-24 11:54:43 +0100

    main

--- tests-files/str/lcat/lcat.c
+++ tests-files/str/lcat/lcat.c
@@ -3,32 +3,20 @@
 
 int main(int ac, char **av)
 {
-	if (ac == 4)
+	if (ac == 3)
 	{
 		int	ft;
 		int	str;
-		char *dst;
-		char *src;
-		char *ft_dst;
-		char *ft_src;
-
-		dst = malloc(sizeof(char) * 25);
-		ft_dst = malloc(sizeof(char) * 25);
-		int i = 0;
-		while (av[1][i])
-		{
-			dst[i] = av[1][i];
-			ft_dst[i] = av[1][i];
-			i++;
-		}
-		dst[i] = '\0';
-		ft_dst[i]= '\0';
+		char dst[25] = av[1];
+		const char *src;
+		char ft_dst[25] = av[1];
+		const char *ft_src;
 
 		src = av[2];
 		ft_src = av[2];
 
-		str = strlcat(dst, src, (size_t)ft_atoi(av[3]));
-		ft = ft_strlcat(ft_dst, ft_src, (size_t)ft_atoi(av[3]));
+		str = strlcat(dst, src, sizeof(dst));
+		ft = ft_strlcat(ft_dst, ft_src, sizeof(ft_dst));
 
 		if (ft != str || strcmp(dst, ft_dst))
 			ft_printf("KO\n");
